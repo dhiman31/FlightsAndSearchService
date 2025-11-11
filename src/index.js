2e439d3793eaf56acfd55f73b70321e2988f33a4
dhiman31 <ankushiiituna@gmail.com>
2025-11-11 22:43:52 +0530

    Added City Model and migration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
 
 const {PORT} = require('./config/ServerConfig');
 
